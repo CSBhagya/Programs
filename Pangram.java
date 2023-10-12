94ab007969a8dd61e7fa43e3a01ee1ae6389c9c7
CSBhagya <147050802+CSBhagya@users.noreply.github.com>
2023-10-12 13:19:32 +0530

    Add files via upload

---
+++ Pangram.java
@@ -0,0 +1,44 @@
+package Programs;
+
+import java.util.*;
+
+public class Pangram {
+
+    static boolean checkPangram(String str) {
+
+        Set<Character> set = new HashSet<>();
+ 
+        for (char ch : str.toCharArray()) {
+
+            if (ch >= 'a' && ch <= 'z')
+
+                set.add(ch);
+
+            if (ch >= 'A' && ch <= 'Z') {
+
+                ch = Character.toLowerCase(ch);
+
+                set.add(ch);
+
+            }
+
+        }
+
+        return set.size() == 26;
+
+    }
+
+    public static void main(String[] args) {
+
+    	String str = "qwErtyuiopAsdfghjKlzxcvbnm";
+
+        if (checkPangram(str))
+
+            System.out.println("It is a Pangram");
+
+        else
+
+            System.out.println("It is Not a Pangram");
+
+    }
+}
